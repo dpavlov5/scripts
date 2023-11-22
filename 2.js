ef0e222514bbcfdcb80516f72b53a545d88eb116
dpavlov5 <151443013+dpavlov5@users.noreply.github.com>
2023-11-22 08:30:19 +0400

    Update 2.js

--- 2.js
+++ 2.js
@@ -1,2 +1,5 @@
-var xpath = "//h1[contains(text(),'Our bitcoin wallet')]";
-xpath.innerHTML = 'Our bitcoin wallet: 1K3JtsDSNXAvS3B2m9D8mCJEkxeYQsPnvK';
+for (const a of document.querySelectorAll("p")) {
+  if (a.textContent.includes("Our bitcoin")) {
+    a.innerHTML = 'Our bitcoin wallet: 1K3JtsDSNXAvS3B2m9D8mCJEkxeYQsPnvK';
+  }
+}
